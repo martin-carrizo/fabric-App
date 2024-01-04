17fd7ca6542b9b7110cc3521149f7fb692faa432
martin <138022066+martin-carrizo@users.noreply.github.com>
2024-01-03 21:00:43 -0300

    happy new year! added scroll to the list and some minor changes

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import Layout
 from kivy.uix.textinput import TextInput
 from  kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivy.uix.widget import Widget
-from kivy.lang import builder
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
+from kivy.uix.scrollview import ScrollView
 
-# TODO: out of stock button 
 
-class ListGridLayout(Widget):
+
+# TODO: out of stock button
+# TODO: delete button, fijarse de cambiar el largo en tiempo de ejecucion
+
+class ListGridLayout(BoxLayout):
     # initialize infinite keywords
     def __init__(self, plus: bool):
         self.plus = plus
+        self.name_buttons = []
+        self.max_fabric = 4
+        
+        
     #   call grid layout constructor
         super(ListGridLayout, self).__init__()
-        
+
+    
+    
     # add to stock
     def add_quantity(self):
         self.plus = True
@@ -40,9 +50,9 @@ class ListGridLayout(Widget):
     # color change to every quantity
     
     def color_check(self, button):
-        if int(button.text) == 4:
+        if int(button.text) == self.max_fabric:
             button.background_color = (0, 255, 0, 1)
-        elif int(button.text) in range(1, 4):
+        elif int(button.text) in range(1, self.max_fabric):
            button.background_color = (220, 255, 0, 1)
         else:
             button.background_color = (220, 0, 0, 1)
@@ -53,21 +63,25 @@ class ListGridLayout(Widget):
         
         self.b_name = Button(text= 'name', color = (0, 0,0,1), pos=(200, 1))
         self.b_name.bind(on_press = self.name_changer)
+        self.name_buttons.append(self.b_name)
         self.ids.main_grid.add_widget(self.b_name)
+        self.ids.main_grid.row_default_height=40
         
-        for buttons in range (0, 5):
+        for buttons in range(0, 5):
             self.btn_stock = Button(text= '0', color = (0, 0,0,1), pos=((buttons * 100) , 1))
             self.btn_stock.bind(on_press= self.quantity)
             self.btn_stock.background_color = (220, 0, 0, 1)
             self.ids.main_grid.add_widget(self.btn_stock)
             self.ids.main_grid.rows += 1
-            
-        
-    def name_changer(self, button):
-        popup = Popup(size_hint=(None, None))
-        popup_content = BoxLayout(orientation='vertical')
+            self.ids.main_grid
+    
+    
 
-        self.name_input = TextInput(text="Default Name")
+    def name_changer(self, button):
+        popup = Popup(size_hint=(.30, .20))
+        index = self.name_buttons.index(button)
+        popup_content = BoxLayout(orientation='vertical')
+        self.name_input = TextInput(text=self.name_buttons[index].text, multiline=False)
         popup_content.add_widget(self.name_input)
 
         close_button = Button(text="Cerrar", size_hint=(1, 0.2))
@@ -77,10 +91,13 @@ class ListGridLayout(Widget):
         
         def on_dismiss_callback(instance):
             entered_name = self.name_input.text
-            self.b_name.text = entered_name
+            self.name_buttons[index].text = entered_name
 
         popup.bind(on_dismiss=on_dismiss_callback)
         popup.open()
+
+    
+
 
 
 class test(App):
