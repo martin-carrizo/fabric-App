b580cddc8a61d1e293b5e4d7a61236ffa46d47c4
martin <138022066+martin-carrizo@users.noreply.github.com>
2024-01-05 17:49:41 -0300

    added delete functional button. the size of the grids need to be fixed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import Layout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from  kivy.uix.label import Label
 from kivy.uix.image import Image
@@ -11,11 +12,15 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.checkbox import CheckBox
+from kivy.clock import Clock
+import uuid
 
 
-
+#TODO: normalize syntaxis
 # TODO: out of stock button
-# TODO: delete button, fijarse de cambiar el largo en tiempo de ejecucion
+# TODO: delete button
+# TODO: comment the code plis
 
 class ListGridLayout(BoxLayout):
     # initialize infinite keywords
@@ -57,25 +62,44 @@ class ListGridLayout(BoxLayout):
         else:
             button.background_color = (220, 0, 0, 1)
     
-        
+            
             
     def add_instance(self, ids):
         
+        self.new_grid = GridLayout(cols= 6, rows= 1, size_hint_y=None)
+        self.new_grid.id = str(uuid.uuid4())
+        self.ids.box_grid.add_widget(self.new_grid)
         self.b_name = Button(text= 'name', color = (0, 0,0,1), pos=(200, 1))
-        self.b_name.bind(on_press = self.name_changer)
+        self.b_name.bind (on_press=self.on_button_press, on_release=self.on_button_release)
         self.name_buttons.append(self.b_name)
-        self.ids.main_grid.add_widget(self.b_name)
-        self.ids.main_grid.row_default_height=40
+        self.new_grid.add_widget(self.b_name)
+        self.new_grid.row_default_height=40
+        
         
         for buttons in range(0, 5):
             self.btn_stock = Button(text= '0', color = (0, 0,0,1), pos=((buttons * 100) , 1))
             self.btn_stock.bind(on_press= self.quantity)
             self.btn_stock.background_color = (220, 0, 0, 1)
-            self.ids.main_grid.add_widget(self.btn_stock)
-            self.ids.main_grid.rows += 1
-            self.ids.main_grid
+            self.new_grid.add_widget(self.btn_stock)
     
     
+    # make a single function from these two
+    def on_button_press(self, instance):
+        # check this latter
+        self.button_index = self.name_buttons.index(instance)
+        self.start_time = Clock.get_time()
+
+    def on_button_release(self, instance):
+        elapsed_time = Clock.get_time() - self.start_time
+        min_time = 0.20
+        if elapsed_time < min_time:
+            self.name_changer(instance)
+        else:
+            
+            parent_id = instance.parent.id
+            self.delete_line_by_id(parent_id)
+
+
 
     def name_changer(self, button):
         popup = Popup(size_hint=(.30, .20))
@@ -84,20 +108,29 @@ class ListGridLayout(BoxLayout):
         self.name_input = TextInput(text=self.name_buttons[index].text, multiline=False)
         popup_content.add_widget(self.name_input)
 
-        close_button = Button(text="Cerrar", size_hint=(1, 0.2))
+        close_button = Button(text="Close", size_hint=(1, 0.2))
         close_button.bind(on_press=lambda instance: popup.dismiss())
         popup_content.add_widget(close_button)
         popup.content = popup_content
         
         def on_dismiss_callback(instance):
-            entered_name = self.name_input.text
-            self.name_buttons[index].text = entered_name
+           self.name_buttons[index].text = self.name_input.text
 
         popup.bind(on_dismiss=on_dismiss_callback)
         popup.open()
+   
+    def delete_line_by_id(self, widget_id):
+        print(widget_id)
+        # search widget by id
+        widget_to_delete = None
+        for widget in self.ids.box_grid.children:
+            if widget.id == widget_id:
+                widget_to_delete = widget
+                break
+        # remove the widget
+        self.ids.box_grid.remove_widget(widget_to_delete)
 
     
-
 
 
 class test(App):
